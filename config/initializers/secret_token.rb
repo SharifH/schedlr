dbb1cf43f75018fe6942a7882c623eb850b5bf77
staxworks <matthewp.owens@gmail.com>
2013-08-02 16:31:45 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schedlr::Application.config.secret_token = 'c6613b8ad1e7c86750137269f9a384c911a785fe39945bbce5a3b6fc7db69efdf8095839b09f02c35375e0dead2e5f614df46ad750332bd2f0cac1f3a3c8d279'
