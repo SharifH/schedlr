a35c9cbd01c3f38934546f7d1e48dc60e7b91303
Donielle Berg <donielle.berg@gmail.com>
2013-08-03 16:18:47 -0700

    add Model/Migrations for Program and Session

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,6 +1,10 @@
 class Session < Program
   attr_accessible :start, :end, :day, :program_id
 
+  validates :start, presence: true
+  validates :end, presence: true
+  validates :program_id, presence: true
+
   belongs_to :program
   has_many :sessions
   has_and_belongs_to_many :skills  
